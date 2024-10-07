fcd4d2659a4baeca4beef7a5d6ea61cbfea33cfc
dev <166125538+Purohitdev@users.noreply.github.com>
2024-10-07 17:45:16 +0530

    marquee done

--- src/Comp/Sec1.jsx
+++ src/Comp/Sec1.jsx
@@ -1,8 +1,110 @@
 import React from 'react'
+import Marquee from "react-fast-marquee";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiNextdotjs } from "react-icons/si";
+import { FaAngular } from "react-icons/fa";
+import { VscVscode } from "react-icons/vsc";
+import { RiJavaLine } from "react-icons/ri";
+import { RiJavascriptFill } from "react-icons/ri";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { SiEclipseide } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import { FaPhp } from "react-icons/fa";
+import { DiRuby } from "react-icons/di";
+import { SiPostman } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { IoLogoFigma } from "react-icons/io5";
+import { SiCanva } from "react-icons/si";
+import { DiDjango } from "react-icons/di";
 
 function Sec1() {
   return (
-    <div className='sec1'></div>
+    <div className='sec1'>
+      <div className="marq1">
+        <Marquee direction='right' speed="40">
+
+          <div className="box-mrq">
+            <RiReactjsLine />
+          </div>
+          <div className="box-mrq">
+          <SiNextdotjs />
+
+          </div>
+          <div className="box-mrq">
+          <FaAngular />
+
+          </div>
+          <div className="box-mrq">
+          <VscVscode />
+          </div>
+          <div className="box-mrq">
+          <RiJavaLine />
+          </div>
+          <div className="box-mrq">
+          <RiJavascriptFill />
+          </div>
+          <div className="box-mrq">
+          <FaHtml5 />
+          </div>
+          <div className="box-mrq">
+          <FaCss3Alt />
+          </div>
+          <div className="box-mrq">
+          <SiTailwindcss />
+          </div>
+          <div className="box-mrq">
+          <FaBootstrap />
+          </div>
+
+
+        </Marquee>
+
+      </div>
+      <div className="marq1">
+
+        <Marquee direction='left' speed="40">
+
+
+          <div className="box-mrq">
+          <FaPython />
+          </div>
+          <div className="box-mrq">
+          <SiEclipseide />
+          </div>
+          <div className="box-mrq">
+          <FaDocker />
+          </div>
+          <div className="box-mrq">
+          <FaPhp />
+          </div>
+          <div className="box-mrq">
+          <DiRuby />
+          </div>
+          <div className="box-mrq">
+          <SiPostman />
+          </div>
+          <div className="box-mrq">
+          <GrMysql />
+          </div>
+          <div className="box-mrq">
+          <IoLogoFigma />
+          </div>
+          <div className="box-mrq">
+          <SiCanva />
+          </div>
+          <div className="box-mrq">
+          <DiDjango />
+          </div>
+
+        </Marquee>
+
+      </div>
+
+    </div>
   )
 }
 
